bff3587e3d92e97a4a8d2ab6bceafcdbaceeb06f
Administrator <administrator@Alisters-iMac.local>
2017-06-16 12:11:20 +0800

    hangman completed

--- Week 3/ps3_hangman.py
+++ Week 3/ps3_hangman.py
@@ -8,6 +8,7 @@
 # (so be sure to read the docstrings!)
 
 import random
+import string
 
 WORDLIST_FILENAME = "words.txt"
 
@@ -78,11 +79,13 @@ def getGuessedWord(secretWord, lettersGuessed):
 def getAvailableLetters(lettersGuessed):
     '''
     lettersGuessed: list, what letters have been guessed so far
-    returns: string, comprised of letters that represents what letters have not
+    returns: string, comprised of letters  represents what letters have not
       yet been guessed.
     '''
     # Get alphabet
     alphabet = string.ascii_lowercase
+    if(lettersGuessed == []):
+        return alphabet
     for ch in lettersGuessed:
         if ch in alphabet:
             alphabet = alphabet.replace(ch,"")
@@ -109,9 +112,53 @@ def hangman(secretWord):
 
     Follows the other limitations detailed in the problem write-up.
     '''
-    # FILL IN YOUR CODE HERE...
-
-
+    # Start game
+    print("Welcome the game, Hangman!")
+    #Annouce length of letters
+    print("I am thinking of a word that is " + str(len(secretWord)) + " letters long.")
+    wordGuess = False
+    amountofGuesses = 8
+    lettersGuessed = []
+    legitletter = string.ascii_lowercase
+    
+    while wordGuess == False and amountofGuesses > 0:
+        print("-------------")
+        print("You have " + str(amountofGuesses) + " left.")
+        print("Available letters: " + getAvailableLetters(lettersGuessed))
+        currentGuess = input("Please guess a letter: ").lower()
+        #First, lets see if its been written already
+        if(currentGuess not in getAvailableLetters(lettersGuessed)):
+            print("Oops! You've already guessed that letter: " + getGuessedWord(secretWord, lettersGuessed))
+            continue
+        elif(currentGuess in legitletter and currentGuess in getAvailableLetters(lettersGuessed)):
+            #means it is a legit letter, and it is NOT been guessed
+            if(currentGuess in secretWord):
+                #menas its in the word
+                lettersGuessed.append(currentGuess)
+                print("Good guess: " + getGuessedWord(secretWord, lettersGuessed))
+                #Check win 
+                if(isWordGuessed(secretWord, lettersGuessed)):
+                    wordGuess = True
+                else:
+                    continue
+                
+            else:
+                #obviously not in the word
+                print("Oops! That letter is not in my word: " + getGuessedWord(secretWord, lettersGuessed))
+                lettersGuessed.append(currentGuess) #add to the list of guesses
+                amountofGuesses = amountofGuesses - 1 #lose a guess
+                continue
+        else:
+            print("That is not a letter!")
+            continue
+    
+    if(amountofGuesses <= 0):
+        #player lost
+        print("-----------")
+        print("Sorry, you ran out of guesses. The word was " + secretWord)
+    if(wordGuess == True):
+        print("-----------")
+        print("Congratulations, you won!")
 
 
 
@@ -120,5 +167,5 @@ def hangman(secretWord):
 # and run this file to test! (hint: you might want to pick your own
 # secretWord while you're testing)
 
-# secretWord = chooseWord(wordlist).lower()
-# hangman(secretWord)
+secretWord = chooseWord(wordlist).lower()
+hangman(secretWord)
